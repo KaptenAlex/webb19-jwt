9f4069dcb35e8fcf2bcc766a75a573fde99a4d52
KaptenAlex <alexander.m.wilson1996@gmail.com>
2020-11-27 10:05:01 +0100

    Added Links and changed to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css';
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Link } from 'react-router-dom'
 import LoginPage from './pages/LoginPage';
 import PrivateRouter from './components/PrivateRouter';
 import PrivatePage from './pages/PrivatePage';
@@ -23,6 +23,17 @@ function App() {
 
   return (
     <div>
+      <ul>
+        <li>
+          <Link to="/login">Login page</Link>
+        </li>
+        <li>
+          <Link to="/private">Private page</Link>
+        </li>
+        <li>
+          <Link to="/404">404 page</Link>
+        </li>
+      </ul>
       <Switch>
         <Route path="/login" component={LoginPage} />
         <PrivateRouter path="/private" component={PrivatePage}/>
